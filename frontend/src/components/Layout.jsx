bbffeeeff08d31b5534e950b1ff569e2e6feeadd
chaeyoonseung <thestar4797@gmail.com>
2025-05-30 11:35:18 +0900

    상단layout 네비게이션 추가가

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,13 +1,27 @@
-import { AppBar, Toolbar, Typography, Container } from '@mui/material';
+import { AppBar, Toolbar, Typography, Container, Button, Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-const Layout = ({ children }) => (
+const Layout = ({ children }) => {
+  
+  const nav = useNavigate();
+  return(
   <>
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6">Book Manager</Typography>
+        <Box>
+            <Button color="inherit" onClick={() => nav('/')}>
+              홈
+            </Button>
+            <Button color="inherit" onClick={() => nav('/books/new')}>
+              도서 등록
+            </Button>
+          </Box>
       </Toolbar>
     </AppBar>
     <Container sx={{ mt: 4 }}>{children}</Container>
   </>
+
 );
+}
 export default Layout;
